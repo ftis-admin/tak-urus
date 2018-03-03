8fe7f8e5123707b703089614139e0761bdb0ccf7
Chris <iam@christianto.net>
2018-03-03 17:05:30 +0700

    reducing email requests

--- app/controller/file.php
+++ app/controller/file.php
@@ -11,23 +11,12 @@ class File {
         } catch (\Exception $e){
             return $f3->error(400, "Token tidak valid. Anda kurang beruntung.");
         }
-        $f3->set('ipinfo',(new \DavidePastore\Ipinfo\Ipinfo(["token"=>$f3->ipinfo['apikey']]))->getFullIpDetails($f3->SERVER['REMOTE_ADDR']));
-        $f3->set('browserinfo', UserAgent::createFromGlobal());
+        $f3->set('ipinfo',$ipfinfo = (new \DavidePastore\Ipinfo\Ipinfo(["token"=>$f3->ipinfo['apikey']]))->getFullIpDetails($f3->SERVER['REMOTE_ADDR']));
+        $f3->set('browserinfo', $browserinfo = UserAgent::createFromGlobal());
         
-        // send email dulu.
-        $mailer = \Model\Email::instance()->createMailer();
-        try {
-            if(!$mailer)
-                return $f3->error(503, "Whoops! Ada pelanggaran keamanan saat mencoba mendownload file. Donwload dibatalkan.");
-            $mailer->addAddress($f3->USER['object']->email, $f3->USER['object']->name);        
-            $mailer->isHTML(true);
-            $mailer->Subject = "Notifikasi Pengunduhan Drive Z";
-            $mailer->Body = \View\Email::render("get-notif.html");
-            $hasil = $mailer->send();
-        } catch (\Exception $e){
-            echo $mailer->ErrorInfo;
-            return $f3->error(503, "Whoops! Ada pelanggaran keamanan saat mencoba mendownload file. Donwload dibatalkan.");
-        }
+        $mail = inform_subject();
+        if($mail !== true)
+            return $mail;
 
         //log si yang mintanya:
         $ret = new \Model\RetriveralLog();
@@ -35,8 +24,8 @@ class File {
             "user"=>$f3->USER['object']->id,
             "type"=>"zip-fetch",
             "detail"=>json_encode([
-                "ipinfo" => $f3->ipinfo,
-                "browserinfo"=>$f3->browserinfo
+                "ipinfo" => $ipinfo,
+                "browserinfo"=>$browserinfo
             ]),
         ]);
         $ret->save();
@@ -49,6 +38,27 @@ class File {
 
 
         \Web::instance()->send($f3->ZZIP . $filename, null, 2048);
+    }
+
+    private function inform_subject($f3){
+        // send email dulu.
+        $mailer = \Model\Email::instance()->createMailer();
+        try {
+            if($f3->exists('SESSION.download_informed'))
+                return true;
+            if(!$mailer)
+                return $f3->error(503, "Whoops! Ada pelanggaran keamanan saat mencoba mendownload file. Donwload dibatalkan.");
+            $mailer->addAddress($f3->USER['object']->email, $f3->USER['object']->name);        
+            $mailer->isHTML(true);
+            $mailer->Subject = "Notifikasi Pengunduhan Drive Z";
+            $mailer->Body = \View\Email::render("get-notif.html");
+            $hasil = $mailer->send();
+            $f3->set('SESSION.download_informed', true);
+        } catch (\Exception $e){
+            echo $mailer->ErrorInfo;
+            return $f3->error(503, "Whoops! Ada pelanggaran keamanan saat mencoba mendownload file. Donwload dibatalkan.");
+        }
+        return true;
     }
 
     public function get_page($f3) {
